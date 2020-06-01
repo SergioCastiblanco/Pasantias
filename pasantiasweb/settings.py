7d2d9b94e9813d352ba18327ae08827cdf9b5ccb
xSACRx <seancaro0217@gmail.com>
2020-05-31 19:26:26 -0500

    second commit

--- pasantiasweb/settings.py
+++ pasantiasweb/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'pasantiasweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pasantias',
+        'USER': 'postgres',
+        'PASSWORD': 'pasantias123',
+        'HOST': 'pasantias.cmyideuqh4jv.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    } 
 }
 
 
